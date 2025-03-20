3496d817bcb4b446c070142729b8edbbe5663c2a
suereact <iamsusanna.b@gmail.com>
2025-03-20 13:11:42 +0100

    added width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-full flex flex-col">
+    <div className="min-h-full flex flex-col p-4 md:p-8 xl:p-12 min-w-80 max-w-[1440px]">
       <header>header</header>
       <main className="flex-[1_1_auto]">main</main>
       <footer>footer</footer>
