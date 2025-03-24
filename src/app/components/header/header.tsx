a463d135920246f46d1c1e88eaf7777e7bdee09d
suereact <iamsusanna.b@gmail.com>
2025-03-24 16:51:41 +0100

    styled navbar

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,25 +1,32 @@
 export const Header = () => {
   return (
     <header>
-      <div className="header_container">
-        <div className="header_menu menu">
-          <button className="menu_icon icon-menu" type="button">
-            <span></span>
-          </button>
+      {/* TODO: fix mobile nav */}
+      <div className="header_container py-9 grid grid-cols-[1fr_auto_1fr] items-center gap-3.5">
+        <div className="header_menu menu justify-self-start self-center">
           <nav className="menu_body">
-            <ul className="menu_list">
+            <ul className="menu_list flex items-center gap-x-20 flex-wrap gap-y-1">
               <li className="menu_item">
-                <a href="" className="menu_link">
+                <a
+                  href=""
+                  className="menu_link text-white font-500 tracking-[-0.4px] leading-5"
+                >
                   Menus
                 </a>
               </li>
               <li className="menu_item">
-                <a href="" className="menu_link">
+                <a
+                  href=""
+                  className="menu_link  text-white font-500 tracking-[-0.4px] leading-5"
+                >
                   About & Careers
                 </a>
               </li>
               <li className="menu_item">
-                <a href="" className="menu_link">
+                <a
+                  href=""
+                  className="menu_link  text-white font-500 tracking-[-0.4px] leading-5"
+                >
                   Contact
                 </a>
               </li>
@@ -28,13 +35,13 @@ export const Header = () => {
         </div>
         <a
           href="#"
-          className="header_logo text-white text-[32px] tracking-[-1.2px]"
+          className="header_logo text-white text-2xl md:text-3xl tracking-[-1.2px]"
         >
           debonaire
         </a>
         <a
           href=""
-          className="header_book book px-[3px] relative text-white leading-1.31 text-center font-500 tracking-tighter after:content-[' '] after:absolute after:inset-0 after:top-[calc(100%+0.34375rem)] after:left-0 after:bg-[#F95454] after:h-0.5 after:w-full"
+          className="header_book book justify-self-end self-center px-[3px] relative text-white leading-1.31 text-center font-500 tracking-tighter after:content-[' '] after:absolute after:inset-0 after:top-[calc(100%+0.34375rem)] after:left-0 after:bg-[#F95454] after:h-0.5 after:w-full"
         >
           Book an Appoitment
         </a>
